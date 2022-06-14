ee83fbbded4e247ac197e6481da24fd851437187
Denis Davydov <zyaalf@yandex.ru>
2022-06-14 17:28:47 +0300

    refact progression

--- src/main/java/hexlet/code/games/Progression.java
+++ src/main/java/hexlet/code/games/Progression.java
@@ -4,29 +4,39 @@ public class Progression {
     public static void runGame() {
         String gameName = "What number is missing in the progression?";
         String[][] pair = Engine.getArray();
-        final int min = 5;
+
         for (var i = 0; i < Engine.numRound(); i++) {
             String answer;
-            int massiveLength = min + (int) (Math.random() * min * 2);
-            int firstNumber = (int) (Math.random() * min * 2);
-            int difference = 1 + (int) (Math.random() * min * 2);
-            String[] progression = new String[massiveLength];
-            progression[0] = String.valueOf(firstNumber);
-            for (var x = 1; x < massiveLength; x++) {
-                progression[x] = String.valueOf((Integer.parseInt(progression[x - 1]) + difference));
-            }
-            int skip = (int) (Math.random() * massiveLength);
+            String[] progression = getProgression();
+            int skip = (int) (Math.random() * progression.length);
             answer = progression[skip];
-            for (var y = 0; y < massiveLength; y++) {
-                if (y == skip) {
-                    progression[y] = "..";
-                }
-            }
-            String str = String.join(" ", progression);
+            String str = getStr(progression, skip);
             pair[i][0] = str;
             pair[i][1] = answer;
         }
         Engine.gameRounds(gameName, pair);
+    }
+    public static String[] getProgression() {
+        final int min = 5;
+        int massiveLength = min + (int) (Math.random() * min * 2);
+        int firstNumber = (int) (Math.random() * min * 2);
+        int difference = 1 + (int) (Math.random() * min * 2);
+        String[] progression = new String[massiveLength];
+        progression[0] = String.valueOf(firstNumber);
+        for (var i = 1; i < massiveLength; i++) {
+            progression[i] = String.valueOf((Integer.parseInt(progression[i - 1]) + difference));
+        }
+        return progression;
+    }
+    public static String getStr(String[] progression, int skip) {
+        String str = "";
+        for (var i = 0; i < progression.length; i++) {
+            if (i == skip) {
+                progression[i] = "..";
+            }
+            str = String.join(" ", progression);
+        }
+        return str;
     }
 }
 
