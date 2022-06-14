74529f8c4727a031216feaa9d3f22df9b415048e
Denis Davydov <zyaalf@yandex.ru>
2022-06-14 17:42:00 +0300

    refact prime

--- src/main/java/hexlet/code/games/Prime.java
+++ src/main/java/hexlet/code/games/Prime.java
@@ -5,23 +5,22 @@ public class Prime {
     public static void runGame() {
         String gameName = "Answer 'yes' if the number is prime, otherwise answer 'no'.";
         String[][] pair = Engine.getArray();
-        int number;
-        int countDivider;
-
-
         for (var i = 0; i < Engine.numRound(); i++) {
-            number = Engine.randomNumber();
-            countDivider = 0;
-            for (int x = 1; x <= number; x++) {
-                if (number % x == 0) {
-                    countDivider++;
-                }
-            }
-            String answer = countDivider == 2 ? "yes" : "no";
+            int number = Engine.randomNumber();
+            int divider = getDivider(number);
+            String answer = divider == 2 ? "yes" : "no";
             pair[i][0] = String.valueOf(number);
             pair[i][1] = answer;
         }
         Engine.gameRounds(gameName, pair);
-
+    }
+    public static int getDivider(int number) {
+        int countDivider = 0;
+        for (int x = 1; x <= number; x++) {
+            if (number % x == 0) {
+                countDivider++;
+            }
+        }
+        return countDivider;
     }
 }
