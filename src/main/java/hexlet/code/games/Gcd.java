ef36ee76ea4fce11503bf514df5786c04d37fceb
Denis Davydov <zyaalf@yandex.ru>
2022-06-14 16:26:26 +0300

    refact gcd 2

--- src/main/java/hexlet/code/games/Gcd.java
+++ src/main/java/hexlet/code/games/Gcd.java
@@ -4,11 +4,12 @@ import hexlet.code.Engine;
 public class Gcd {
     public static void runGame() {
         String gameName = "Find the greatest common divisor of given numbers.";
-        String[][] pair = Engine.getArray();
+        String[][] pair = new String[Engine.numRound()][2];
         int answer = 1;
+        final int max = 100;
         for (var i = 0; i < Engine.numRound(); i++) {
-            int a = 1 + Engine.randomNumber();
-            int b = 1 + Engine.randomNumber();
+            int a = 1 + (int) (Math.random() * max);
+            int b = 1 + (int) (Math.random() * max);
             for (int x = 1; x <= a && x <= b; x++) {
                 if (a % x == 0 && b % x == 0) {
                     answer = x;
